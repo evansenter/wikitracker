c9d659c158940c513a0e675f376d581d18c26597
Donnie Tognazzini <dtognazzini@donnie-tognazzinis-macbook-pro.local>
2009-07-07 18:46:16 -0700

    Started browser implementation...

--- config/routes.rb
+++ config/routes.rb
@@ -30,10 +30,9 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => 'browser', :action => 'index'
 
-  # See how all your routes lay out with "rake routes"
+  map.resource :browser
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
